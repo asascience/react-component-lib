af5ca4147c82ee040ca2bd41920dddb6963d1813
Jake Polatty <jakepolatty@gmail.com>
2018-07-10 16:28:16 -0400

    Added snackbar component to display errors

--- src/components/LeafletMapWithWMSControls/LeafletMapWithWMSControls.js
+++ src/components/LeafletMapWithWMSControls/LeafletMapWithWMSControls.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import LeafletMap from '../LeafletMap/LeafletMap';
 import LeafletWMSControls from '../LeafletWMSControls/LeafletWMSControls';
+import Snackbar from 'material-ui/Snackbar';
 
 import './LeafletMapWithWMSControls.css';
 
@@ -13,6 +14,8 @@ class LeafletMapWithWMSControls extends Component {
       opacity: 1,
       layerIndex: 0,
       styleIndex: 0,
+      errorOpen: false,
+      errorUrl: '',
     };
 
     this.onCheckTransparent = this.onCheckTransparent.bind(this);
@@ -20,6 +23,8 @@ class LeafletMapWithWMSControls extends Component {
     this.onLayerChange = this.onLayerChange.bind(this);
     this.onStyleChange = this.onStyleChange.bind(this);
     this.onLoadError = this.onLoadError.bind(this);
+    this.handleErrorActionClick = this.handleErrorActionClick.bind(this);
+    this.closeErrorSnackbar = this.closeErrorSnackbar.bind(this);
   }
 
   onCheckTransparent(isChecked) {
@@ -48,7 +53,21 @@ class LeafletMapWithWMSControls extends Component {
   }
 
   onLoadError(error) {
-    console.log('error: ', error)
+    this.setState({
+      errorUrl: error.tile.currentSrc,
+      errorOpen: true,
+    });
+  }
+
+  closeErrorSnackbar() {
+    this.setState({
+      errorOpen: false,
+      errorMessage: '',
+    });
+  }
+
+  handleErrorActionClick() {
+    window.open(this.state.errorUrl, '_blank');
   }
 
   render() {
@@ -59,46 +78,56 @@ class LeafletMapWithWMSControls extends Component {
     let styles = this.props.layerData[this.state.layerIndex].styles;
 
     return (
-      <div className='leaflet-map-wms-wrapper'>
-        <LeafletMap
-          center={[0, 0]}
-          zoomLevel={2}
-          rasterLayerData={[
-            {
-              type: 'WMS',
-              url: 'http://174.67.104.8/wms/',
-              layers: layers[this.state.layerIndex],
-              styles: styles[this.state.styleIndex],
-              format:  'image/png',
-              transparent: this.state.transparent,
-              opacity: this.state.opacity,
-              version: '1.3.0',
-              time: '2018-04-07T00:00:00Z',
-            }
-          ]}
-          onLoadError={(error) => this.onLoadError(error)}
-        />
-        <LeafletWMSControls
-          transparentField={{
-            isChecked: this.state.transparent,
-            isDisabled: false,
-            onCheck: (e, isChecked) => this.onCheckTransparent(e, isChecked),
-          }}
-          opacityField={{
-            value: this.state.opacity,
-            isDisabled: false,
-            onChange: (newOpacity) => this.onOpacityChange(newOpacity),
-          }}
-          layerField={{
-            value: this.state.layerIndex,
-            layers: layers,
-            onChange: (newIndex) => this.onLayerChange(newIndex),
-          }}
-          styleField={{
-            value: this.state.styleIndex,
-            styles: styles,
-            onChange: (newIndex) => this.onStyleChange(newIndex),
-          }}
+      <div>
+        <div className='leaflet-map-wms-wrapper'>
+          <LeafletMap
+            center={[0, 0]}
+            zoomLevel={2}
+            rasterLayerData={[
+              {
+                type: 'WMS',
+                url: 'http://174.67.104.8/wms/',
+                layers: layers[this.state.layerIndex],
+                styles: styles[this.state.styleIndex],
+                format:  'image/png',
+                transparent: this.state.transparent,
+                opacity: this.state.opacity,
+                version: '1.3.0',
+                time: '2018-04-07T00:00:00Z',
+              }
+            ]}
+            onLoadError={(error) => this.onLoadError(error)}
+          />
+          <LeafletWMSControls
+            transparentField={{
+              isChecked: this.state.transparent,
+              isDisabled: false,
+              onCheck: (e, isChecked) => this.onCheckTransparent(e, isChecked),
+            }}
+            opacityField={{
+              value: this.state.opacity,
+              isDisabled: false,
+              onChange: (newOpacity) => this.onOpacityChange(newOpacity),
+            }}
+            layerField={{
+              value: this.state.layerIndex,
+              layers: layers,
+              onChange: (newIndex) => this.onLayerChange(newIndex),
+            }}
+            styleField={{
+              value: this.state.styleIndex,
+              styles: styles,
+              onChange: (newIndex) => this.onStyleChange(newIndex),
+            }}
+          />
+        </div>
+        <Snackbar
+          open={this.state.errorOpen}
+          message='There was an error loading the WMS data for the specified layer and styles.'
+          action='view error'
+          autoHideDuration={10000}
+          onActionClick={this.closeErrorSnackbar}
+          onRequestClose={this.closeErrorSnackbar}
         />
       </div>
     );
