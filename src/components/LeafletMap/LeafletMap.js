f1d64940ef2bddc0bce0897b83ee04a8bbd7aeff
Jake Polatty <jakepolatty@gmail.com>
2018-07-19 14:58:48 -0400

    Added functionality to access leaflet reference for tile data

--- src/components/LeafletMap/LeafletMap.js
+++ src/components/LeafletMap/LeafletMap.js
@@ -9,10 +9,48 @@ import './LeafletMap.css';
 class LeafletMap extends Component {
   constructor(props) {
     super(props);
+
+    this.mapRef = React.createRef();
+    this.getTileUrl = this.getTileUrl.bind(this);
+  }
+
+  getTileUrl() {
+    let tileUrl = '';
+    let baseUrl = this.props.rasterLayerData[0].url;
+    // if (baseUrl !== '' && this.mapRef.current.leafletElement) {
+    //   let leafletElement = this.mapRef.current.leafletElement;
+    //   let layers = leafletElement['_layers'];
+    //   let layerKeys = Object.keys(layers);
+    //   let wmsLayer = {};
+
+    //   for (let i = 0; i < layerKeys.length; i++) {
+    //     let layer = layers[layerKeys][i];
+    //     if (layer.url === baseUrl) {
+    //       wmsLayer = layer;
+    //     }
+    //   }
+
+    //   tileUrl = wmsLayer;
+    // }
+
+    if (this.mapRef && this.mapRef.current) {
+      tileUrl = this.mapRef.current.leafletElement;
+    }
+
+    return tileUrl;
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.rasterLayerData &&
+      prevProps.rasterLayerData[0].styles !== this.props.rasterLayerData[0].styles) {
+      let tileUrl = this.getTileUrl();
+      this.props.setTileUrl(tileUrl);
+    }
   }
 
   render() {
     let markers  = <div/>;
+
     if (this.props.markerData) {
       markers = (
         <LeafletPane
@@ -61,7 +99,7 @@ class LeafletMap extends Component {
 
     return (
       <div id="map-container">
-        <Map id="leaflet-map" center={this.props.center} zoom={this.props.zoomLevel}>
+        <Map id="leaflet-map" center={this.props.center} zoom={this.props.zoomLevel} ref={this.mapRef}>
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
